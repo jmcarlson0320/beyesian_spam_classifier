731995e4d74833a5dc84ad9d148e4bc3b790979b
Joshua Carlson <jmcarlson0320@gmail.com>
2021-05-17 08:54:11 -0700

    final version

--- project2.py
+++ project2.py
@@ -1,14 +1,19 @@
 # ml_programming_2
 # Joshua Carlson
 # 5/13/21
-import bayes
 import statistics
 import math
 import sys
 import numpy as np
 from enum import IntEnum
 
+NUM_FEATURES = 57
 MIN_FLOAT = sys.float_info.min
+
+
+######################################################################
+# load and process data                                              #
+######################################################################
 
 # load and shuffle data
 data = np.loadtxt(open('data/spambase.data'), delimiter=',')
@@ -25,8 +30,11 @@ y_train = test[:,-1]
 x_test = train[:,:-1]
 y_test = train[:,-1]
 
-# compute the model using training data
-#       Compute prior for each class
+######################################################################
+# compute the model using training data                              #
+######################################################################
+
+#compute priors
 num_spam = np.count_nonzero(y_train)
 prior_spam = num_spam / y_train.shape[0]
 prior_notspam = 1 - prior_spam
@@ -40,15 +48,17 @@ class Stat(IntEnum):
     MEAN = 0
     STDEV = 1
 
-# this holds all the stats calculated in training, model[feature][class] returns a
+# this 2d array holds all the stats calculated in training, model[feature][class] returns a
 # tuple containing (mean, stdev)
-model = np.zeros((57, 2), dtype = ('float', 2))
+model = np.zeros((NUM_FEATURES, 2), dtype = ('float', 2))
+
 # This can't be the most efficient way to do this... but I want to get something working first
-for i in range(57):
+for i in range(NUM_FEATURES):
     # these lists hold the feature data, split into classes
     # mean and stdev are calculated on these
     spam = []
     notspam = []
+
     # fill in the spam/notspam data lists
     for row in train:
         if row[-1] == 1:
@@ -69,9 +79,12 @@ for i in range(57):
         model[i][Class.NOTSPAM][Stat.STDEV] = 0.0001
 
 
-# run the classifier on the test data
-# use the log of the products to avoid underflow
-num_correct = 0 # for calculating accuracy
+######################################################################
+# run the classifier on the test data                                #
+######################################################################
+cnf_matrix = [[0, 0],
+              [0, 0]]
+
 for data_point, target_class in zip(x_test, y_test):
 
     prediction = [0, 0] # [spam, notspam]
@@ -79,7 +92,7 @@ for data_point, target_class in zip(x_test, y_test):
     # this repeated code should be a function...
     # spam
     prediction[Class.SPAM] = math.log(prior_spam)
-    for i in range(57):
+    for i in range(NUM_FEATURES):
         x = data_point[i]
         u = model[i][Class.SPAM][Stat.MEAN]
         s = model[i][Class.SPAM][Stat.STDEV]
@@ -91,7 +104,7 @@ for data_point, target_class in zip(x_test, y_test):
 
     # notspam
     prediction[Class.NOTSPAM] = math.log(prior_notspam)
-    for i in range(57):
+    for i in range(NUM_FEATURES):
         x = data_point[i]
         u = model[i][Class.NOTSPAM][Stat.MEAN]
         s = model[i][Class.NOTSPAM][Stat.STDEV]
@@ -102,14 +115,32 @@ for data_point, target_class in zip(x_test, y_test):
         prediction[Class.NOTSPAM] += p_x_c
         
     predicted_class = np.argmax(prediction)
+
     # needed because I did the enum backwards.. .will fix
     if predicted_class == Class.SPAM:
         predicted_class = 1
     else:
         predicted_class = 0
 
-    if predicted_class == target_class:
-        num_correct += 1
 
-print('accuracy on the test set:')
-print(num_correct / x_test.shape[0])
+
+    # build confusion matrix
+    predicted_class = round(predicted_class)
+    target_class = round(target_class)
+    cnf_matrix[target_class][predicted_class] += 1
+
+
+######################################################################
+# compute summary statistics on the results                          #
+######################################################################
+
+print('confusion matrix:')
+print(cnf_matrix)
+total = cnf_matrix[0][0] + cnf_matrix[0][1] + cnf_matrix[1][0] + cnf_matrix[1][1]
+accuracy = (cnf_matrix[0][0] + cnf_matrix[1][1]) / total
+recall = cnf_matrix[1][1] / (cnf_matrix[1][1] + cnf_matrix[1][0])
+precision = cnf_matrix[1][1] / (cnf_matrix[1][1] + cnf_matrix[0][1])
+
+print('accuracy: ' + str(accuracy))
+print('recall: ' + str(recall))
+print('precision: ' + str(precision))
